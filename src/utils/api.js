dd45a44e8bcd45271a92ac22e9e6b6f5eb4510a7
Kundal7 <vikaskundal74@gmail.com>
2026-01-14 21:58:32 +1100

    Update API base URL to Render backend
    - Change API base URL from localhost:8000 to Render deployment
    - Update to https://todo-mern-backend-ldod.onrender.com
    - All API calls now connect to deployed backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://todo-mern-backend-ldod.onrender.com';
 
 // Create axios instance
 const api = axios.create({
